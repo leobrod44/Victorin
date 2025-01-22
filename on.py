b47b2959296e37f33eccc627d5f527f4627ba9f5
Leo Brodeur <leobrod44@gmail.com>
2025-01-22 13:11:15 -0500

    on

--- on.py
+++ on.py
@@ -7,7 +7,7 @@ GPIO.setmode(GPIO.BCM)
 #removing the warings 
 GPIO.setwarnings(False)
 #creating a list (array) with the number of GPIO's that we use 
-pins = [18,17,15,14, 22, 27] 
+pins = [14,16] 
 
 #setting the mode for all pins so all will be switched on 
 GPIO.setup(pins, GPIO.OUT)
