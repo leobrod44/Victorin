ce1eafb3d73ecdca8f3193bc4ad8f6eb0d81c41f
Leo Brodeur <leobrod44@gmail.com>
2025-01-21 22:10:14 -0500

    2 pin test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     #removing the warings 
     GPIO.setwarnings(False)
     #creating a list (array) with the number of GPIO's that we use 
-    pins = [18,17,15,14] 
+    pins = [18,17,15,14, 22, 27] 
 
     #setting the mode for all pins so all will be switched on 
     GPIO.setup(pins, GPIO.OUT)
@@ -23,27 +23,18 @@ def main():
         GPIO.output(pin,  GPIO.HIGH)
         #wait 0,5 second
         time.sleep(0.5)
+        if not GPIO.input(pin) : 
+            print("Pin "+str(pin)+" is working" )
+
+    #for loop where pin = 18 next 17 ,15, 14 
+    for pin in pins :
         #setting the GPIO to LOW or 0 or false
         GPIO.output(pin,  GPIO.LOW)
         #wait 0,5 second
         time.sleep(0.5)
-
-        #Checking if the current relay is running and printing it 
         if not GPIO.input(pin) : 
             print("Pin "+str(pin)+" is working" )
             
-
-    #same but the difference is that  we have 
-    #for loop where pin = 14 next 15,17,18
-    # backwards
-    for pin in reversed(pins) :
-        GPIO.output(pin,  GPIO.HIGH)
-        time.sleep(0.5)
-
-        GPIO.output(pin,  GPIO.LOW)
-        time.sleep(0.5)
-
-
     #cleaning all GPIO's 
     GPIO.cleanup()
     print("Shutdown All relays")
