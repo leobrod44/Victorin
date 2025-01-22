3b8d9544fe3e334c001fb56fc37a61f85d9c8977
Leo Brodeur <leobrod44@gmail.com>
2025-01-21 22:41:13 -0500

    on off

---
+++ off.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+# getting the time libraly
+import time
+
+# setting a current mode
+GPIO.setmode(GPIO.BCM)
+#removing the warings 
+GPIO.setwarnings(False)
+#creating a list (array) with the number of GPIO's that we use 
+pins = [18,17,15,14, 22, 27] 
+
+#setting the mode for all pins so all will be switched on 
+GPIO.setup(pins, GPIO.OUT)
+
+while True:
+    for pin in pins:
+        GPIO.output(pin,  GPIO.LOW)
+
+      
