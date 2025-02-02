e46d4389a1a7347a053780bce49cf0f9d676baf1
Leo Brodeur <leobrod44@gmail.com>
2025-02-01 20:17:50 -0500

    mv rust

--- IOS/Victorin/ContentView.swift
+++ IOS/Victorin/ContentView.swift
@@ -6,18 +6,19 @@ struct ContentView: View {
         Plant(device: 1, name: "Pothos", cycle: 7, duration: 10),
         Plant(device: 2, name: "Fern", cycle: 5, duration: 5)
     ]
+    
     @State private var isEditing = false
     @State private var isAdding = false // To track if we are adding a new plant
     @State private var selectedPlantIndex: Int? = nil
     @State private var newPlantName: String = "" // State for new plant form fields
     @State private var newPlantCycle: Int = 1
     @State private var newPlantDuration: Int = 5
-
+    
     let columns = [
         GridItem(.flexible(), spacing: 10),
         GridItem(.flexible(), spacing: 10)
     ]
-
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -28,12 +29,12 @@ struct ContentView: View {
                                 self.selectedPlantIndex = index
                                 self.isEditing = true
                             })
-                                .frame(minHeight: 250)
+                            .frame(minHeight: 250)
                         }
                     }
                     .padding()
                 }
-
+                
                 // Add button at bottom right
                 VStack {
                     Spacer()
@@ -61,28 +62,30 @@ struct ContentView: View {
             }
             .sheet(isPresented: $isEditing) {
                 if let index = selectedPlantIndex {
-//                    EditPlantForm(plant: $plants[index])
+                    //                    EditPlantForm(plant: $plants[index])
                 }
             }
         }
     }
-
+    
     private func clearAddPlantFields() {
         newPlantName = ""
         newPlantCycle = 1
         newPlantDuration = 5
     }
     
-    
 }
 
 
 struct PlantView: View {
     @Binding var plant: Plant
-    var onEdit: () -> Void // Closure to handle edit
-
+    var onEdit: () -> Void // Closure to handle edit'
+    
+    @ObservedObject private var webSocketManager = WebSocketManager.shared
+    
     @State private var isWatering = false // Tracks watering state for the animation
-
+    @State private var humidity: Double = 0.0 // Initial humidity value
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
@@ -91,9 +94,9 @@ struct PlantView: View {
                         .font(.headline)
                         .padding(.top, 5)
                         .frame(maxWidth: .infinity, alignment: .center)
-
+                    
                     Spacer()
-
+                    
                     Button(action: onEdit) {
                         Image(systemName: "ellipsis")
                             .foregroundColor(.white)
@@ -105,7 +108,7 @@ struct PlantView: View {
                     .padding(.top, 4)
                     .padding(.trailing, 2)
                 }
-
+                
                 ZStack {
                     HStack {
                         Spacer()
@@ -118,14 +121,20 @@ struct PlantView: View {
                         Spacer()
                     }
                 }
-
+                
                 Spacer()
-
+                
                 HStack {
-                    Circle()
-                        .fill(Color.cyan)
-                        .frame(width: 20, height: 20)
-
+                    ZStack {
+                        Circle()
+                            .fill(Color.cyan)
+                            .frame(width: 50, height: 50) // Increased size for better readability
+                        
+                        Text("\(humidity)%") // Display humidity inside the circle
+                            .font(.caption)
+                            .foregroundColor(.white)
+                    }
+                    
                     Button(action: {
                         triggerWatering(plant: plant)
                     }) {
@@ -149,30 +158,40 @@ struct PlantView: View {
             .overlay(
                 RoundedRectangle(cornerRadius: 15)
                     .stroke(Color.gray, lineWidth: 1)
-            )
+            ).onAppear {
+                // Update the humidity from the WebSocket when the view appears
+                updateHumidity()
+            }
+            .onChange(of: webSocketManager.humidityUpdates) { _ in
+                updateHumidity()
+            }
         }
         .frame(minHeight: 250)
     }
-
+    
+    func updateHumidity() {
+        if let updatedHumidity = webSocketManager.humidityUpdates[plant.device] {
+            humidity = updatedHumidity
+        }
+    }
+    
     func triggerWatering(plant: Plant) {
         
-        
-
         guard let url = URL(string: "http://127.0.0.1:3030/water_plant") else {
             print("Invalid URL")
             isWatering = false
             return
         }
-
+        
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
+        
         let body: [String: Any] = [
             "id": plant.device,
             "name": plant.name
         ]
-
+        
         do {
             request.httpBody = try JSONSerialization.data(withJSONObject: body, options: [])
         } catch {
@@ -180,7 +199,7 @@ struct PlantView: View {
             isWatering = false
             return
         }
-
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             DispatchQueue.main.async {
                 if let error = error {
@@ -188,7 +207,7 @@ struct PlantView: View {
                 } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
                     isWatering = true // Start loading animation
                     print("Watering triggered successfully!")
-
+                    
                     // Simulate waiting for the Raspberry Pi response
                     DispatchQueue.main.asyncAfter(deadline: .now() + Double(plant.duration)) {
                         isWatering = false // End loading animation
@@ -207,13 +226,13 @@ struct PlantView: View {
 struct AddPlantForm: View {
     @Binding var isAdding: Bool
     @Binding var plants: [Plant]
-
+    
     @State private var newPlantName: String = "Plant"
     @State private var newDevice: String = ""
     @State private var parsedDevice: Int? = nil
     @State private var newPlantCycle: Int = 7
     @State private var newPlantDuration: Int = 10
-
+    
     var body: some View {
         NavigationView {
             Form {
@@ -221,23 +240,23 @@ struct AddPlantForm: View {
                     TextField("Name:", text: $newPlantName)
                     
                     TextField("Device:", text: Binding(
-                         get: {
-                             newDevice
-                         },
-                         set: { newValue in
-                             newDevice = newValue
-                             if let intValue = Int(newValue) {
-                                 parsedDevice = intValue
-                             } else {
-                                 parsedDevice = nil
-                             }
-                         }
-                     ))
-
+                        get: {
+                            newDevice
+                        },
+                        set: { newValue in
+                            newDevice = newValue
+                            if let intValue = Int(newValue) {
+                                parsedDevice = intValue
+                            } else {
+                                parsedDevice = nil
+                            }
+                        }
+                    ))
+                    
                     // Stepper for Watering Cycle (in days)
                     Stepper("Watering Cycle: \(newPlantCycle) days", value: $newPlantCycle, in: 1...60)
                         .padding(.vertical, 5)
-
+                    
                     // Stepper for Duration (in seconds)
                     Stepper("Duration: \(newPlantDuration) seconds", value: $newPlantDuration, in: 1...3600)
                         .padding(.vertical, 5)
@@ -272,5 +291,4 @@ struct Plant: Identifiable {
     var humidity: Double = 0.0 // New property for humidity
     var isWatering: Bool = false // New property for loading state
 }
-
 
